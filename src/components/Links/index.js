134a8f200dd18be07c896732c44cc83be91f078c
Caio_Tozer <81998365+CaioTozer@users.noreply.github.com>
2023-07-08 13:25:11 -0300

    Update index.js
    Fix: work update

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -5,7 +5,7 @@ const Links = () => {
     return (
         <div className='links'>
             <p className='pais'><div>Brasil</div></p>
-            <p className='trabalho'><div>UFRPE</div></p>
+            <p className='trabalho'><div>UFRPE/Pagô</div></p>
             <p className='github'><a href='https://github.com/CaioTozer' target='blank'>CaioTozer</a></p>
             <p className='insta'><a href='https://www.instagram.com/caio_tozer/' target='blank'>Caio_Tozer</a></p>
             <p className='linkedin'><a href='https://www.linkedin.com/in/caio-tozer-a77366263/' target='blank'>Caio Tozer</a></p>
