452bbaea6184496ffa83c280bf4bfb3611a3328b
Caio_Tozer <81998365+CaioTozer@users.noreply.github.com>
2024-02-24 16:44:17 -0300

    Atualizar o index.js

--- src/components/Links/index.js
+++ src/components/Links/index.js
@@ -5,7 +5,7 @@ const Links = () => {
     return (
         <div className='links'>
             <p className='pais'><div>Brasil</div></p>
-            <p className='trabalho'><a href='https://eaipago.com><div>Pagô</div></a></p>
+            <p className='trabalho'><a href='https://eaipago.com' target='blank'><div>Pagô</div></a></p>
             <p className='github'><a href='https://github.com/CaioTozer' target='blank'>CaioTozer</a></p>
             <p className='insta'><a href='https://www.instagram.com/caio_tozer/' target='blank'>Caio_Tozer</a></p>
             <p className='linkedin'><a href='https://www.linkedin.com/in/caio-tozer-a77366263/' target='blank'>Caio Tozer</a></p>
